c882174259cb09ed472c639828d41e92a5f73324
Pelmeshka69 <133804012+Pelmeshka69@users.noreply.github.com>
2023-09-12 20:57:38 +0300

    12.09.23

---
+++ 12.09.23(2)/12.09.23(2)/12.09.23(2).cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+};
+
+int main() {
+    Student newList[3]{ {"Vlad",20},
+                       {"Oleg", 15},
+                       {"Maxim",18} };
+
+    int sortList = sizeof(newList) / sizeof(newList[0]);
+
+    for (int i = 0; i < sortList - 1; ++i) {
+        for (int j = 0; j < sortList - i - 1; ++j) {
+            if (newList[j].age > newList[j + 1].age) {
+                swap(newList[j], newList[j + 1]);
+            }
+        }
+    }
+    for (int i = 0; i < sortList; ++i) {
+        cout << "Name: " << newList[i].name << ", Age: " << newList[i].age << '\n';
+    }
+
+    return 0;
+}
---
+++ 12.09.23/12.09.23/12.09.23.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+class Student {
+public:
+    string name;
+    int age;
+};
+
+Student oldStudent(Student students[], int size) {
+    Student oldest = students[0];
+    for (int i = 1; i < size; ++i) {
+        if (students[i].age > oldest.age) {
+            oldest = students[i];
+        }
+    }
+    return oldest;
+}
+
+int main() {
+    Student newList[3]{ {"Vlad",20},
+                       {"Oleg", 19},
+                       {"Maxim",18} };
+    Student oldestStudent = oldStudent(newList, 3);
+
+    cout << "Oldest student : " << oldestStudent.name << '\n';
+    cout << "age: " << oldestStudent.age;
+
+    return 0;
+}
