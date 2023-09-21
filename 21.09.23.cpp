3ff3302325ba759910c359cffa28a6018cf31494
Pelmeshka69 <133804012+Pelmeshka69@users.noreply.github.com>
2023-09-21 19:42:03 +0300

    Update 21.09.23.cpp

--- 21.09.23.cpp
+++ 21.09.23.cpp
@@ -1,56 +1,67 @@
-﻿#include <iostream>
+#include <iostream>
 #include <string>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 
 class BankAccount {
 private:
     string Name;
-    int Number;
+    int id;
     double balance;
-
-public:
-    BankAccount(const string& name, int number, double initialBalance) {
-        Name = name;
-        Number = number;
-        balance = initialBalance;
+    static int generateId() {
+        static int nextId = 1;
+        return nextId++;
     }
 
+
+public:
+    BankAccount(const string& name, double initialBalance) {
+        Name = name;
+        id = generateId();
+        balance = initialBalance;
+    }
     void Deposit(double amount) {
         if (amount > 0) {
             balance += amount;
-            cout << "Внесено на рахунок: " << amount << endl;
+            cout << "Submitted to balance: " << amount << endl;
+        }
+        else {
+            cout << "Error: The contribution amount must be greater than 0." << endl;
         }
 
     }
     void Withdraw(double amount) {
         if (amount > 0 && balance >= amount) {
             balance -= amount;
-            cout << "Знято з рахунку:" << amount << endl;
+            cout << "Withdrawn from the balance:" << amount << endl;
+        }
+        else {
+            cout << "The withdrawal amount is too large." << endl;
         }
     }
-
     int GetBalance() {
         return balance;
     }
-
     void DisplayInfo() {
-        cout << "Ім'я власника: " << Name << endl;
-        cout << "Номер рахунку: " << Number << endl;
-        cout << "Поточний баланс: " << balance << " грн" << endl;
+        cout << "Owner Name: " << Name << endl;
+        cout << "Account number: " << id << endl;
+        cout << "Current balance: " << balance << endl;
     }
 };
 
 int main() {
-
-    BankAccount account1("John", 12345, 1000.0);
-    BankAccount account2("Jack", 54321, 500.0);
+    srand(time(0));
+    BankAccount account1("John", 1000.0);
     account1.DisplayInfo();
-    account2.DisplayInfo();
     account1.Deposit(500.0);
     account1.Withdraw(200.0);
-    account2.Deposit(100.0);
-    account2.Withdraw(800.0);
     account1.DisplayInfo();
+    BankAccount account2("Jack", 500.0);
     account2.DisplayInfo();
+    account2.Deposit(100.0);
+    account2.Withdraw(300.0);
+    account2.DisplayInfo();
+
     return 0;
 }
