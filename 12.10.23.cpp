782f38fa666c1c34f18fa0e44acbdc04d35ef51f
Pelmeshka69 <133804012+Pelmeshka69@users.noreply.github.com>
2023-10-12 21:02:04 +0300

    Create 12.10.23.cpp

---
+++ 12.10.23.cpp
@@ -0,0 +1,72 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Field {
+public:
+    char** array = new char* [10];
+    Field() {
+        for (int i = 0; i < 10; i++) {
+            this->array[i] = new char[10];
+            for (int j = 0; j < 10; j++) {
+                this->array[i][j] = ' ';
+            }
+        }
+    }
+    void showField() {
+        for (int i = 0; i < 30; i++) {
+            cout << '-';
+        }
+        cout << endl;
+        for (int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                cout << '|' << array[i][j] << '|';
+            }
+            cout << endl;
+        }
+        for (int i = 0; i < 30; i++) {
+            cout << '-';
+        }
+    }
+};
+
+void drawLine(Field& field) {
+    for (int i = 0; i < 4; i++) {
+        field.array[i][i] = 'A';
+    }
+}
+
+void drawTank(Field& field) {
+    for (int i = 0; i < 3; i++) {
+        field.array[4][i] = 'B';
+        field.array[5][i] = 'B';
+    }
+}
+
+void drawUpsideDown(Field& field) {
+    for (int i = 0; i < 2; i++) {
+        field.array[9 - i][i] = 'D';
+    }
+}
+
+void drawBox(Field& field) {
+    for (int i = 3; i < 4; i++) {
+        for (int j = 3; j < 4; j++) {
+            field.array[i][j] = 'C';
+        }
+    }
+}
+
+void drawLightning(Field& field) {
+    for (int i = 0; i < 2; i++) {
+        field.array[i][4 - i] = 'E';
+    }
+}
+
+int main() {
+    Field field;
+
+    drawLightning(field);
+
+    field.showField();
+}
